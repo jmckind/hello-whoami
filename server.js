22c7022032361f3d27247a6ac477083e02fe5633
John McKenzie <jmckind@gmail.com>
2017-08-24 13:27:14 -0500

    Clean up code format and remove redundant console logging

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const settings = require('./package.json');
 
 var server = http.createServer(function(request, response) {
   console.log(request.method + " " + request.url + " " + request.headers['user-agent']);
-console.log(settings.version);
+
   response.setHeader('ETag', etag(uuidv4()));
   response.writeHead(200, {"Content-Type": "text/plain"});
+
   response.write("Hello, I am " + process.env.HOSTNAME + "\n");
   response.write("I am running version " + settings.version + "\n");
+
   response.end();
 });
 
